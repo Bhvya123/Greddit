f5270af16a90368fc6c950244b332fc0ed34edf7
Bhvya123 <bhvyakothari13@gmail.com>
2023-08-21 13:23:10 +0530

    Mongo link update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongoose');
 
 const connectDB=async()=>{
     try{
-        const conn= await MongoClient.connect("mongodb+srv://vyom:vyom2003@dass-1.uq862gi.mongodb.net/Greddiit?retryWrites=true&w=majority")
+        const conn= await MongoClient.connect("mongodb+srv://admin:admin@dass-1.uq862gi.mongodb.net/Greddiit?retryWrites=true&w=majority")
         console.log(`MongoDB connected: ${conn.connection.host}`)
     }catch(error)
     {
@@ -11,3 +11,5 @@ const connectDB=async()=>{
 }
 
 module.exports= connectDB;
+
+// mongodb+srv://admin:<password>@merncluster.e7mz7bn.mongodb.net/?retryWrites=true&w=majority
